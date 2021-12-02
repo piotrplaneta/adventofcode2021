5b1ef34cfaa6188a4ba3f499e2b9dcd4f640aa30
piotrplaneta <piotr.planeta@gmail.com>
2021-12-02 14:00:20 +0100

    Add day02 solution

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -1,12 +1,12 @@
 import java.io.File
 
-fun input(): List<Int> = File("inputs/day01").bufferedReader().readLines().map { it.toInt() }
+private fun input(): List<Int> = File("inputs/day01").bufferedReader().readLines().map { it.toInt() }
 
-fun partOne(): Int {
+private fun partOne(): Int {
     return input().zip(input().drop(1)).map { (first, second) -> second > first }.count { it }
 }
 
-fun partTwo(): Int {
+private fun partTwo(): Int {
     val triplets = input().zip(input().drop(1)).zip(input().drop(2)).map {
         listOf(it.first.first, it.first.second, it.second)
     }
@@ -14,7 +14,7 @@ fun partTwo(): Int {
     return triplets.zip(triplets.drop(1)).map { (first, second) -> second.sum() > first.sum() }.count { it }
 }
 
-fun main(args: Array<String>) {
+private fun main(args: Array<String>) {
     println(partOne())
     println(partTwo())
 }
---
+++ src/main/kotlin/day02.kt
@@ -0,0 +1,34 @@
+import java.io.File
+
+private fun input(): List<Pair<String, Int>> {
+    return File("inputs/day02").bufferedReader().readLines().map {
+        Pair(it.split(" ")[0], it.split(" ")[1].toInt())
+    }
+}
+
+private fun partOne(): Pair<Int, Int> {
+    return input().fold(Pair(0, 0)) { (acc_f, acc_d), (move, amount) ->
+        when(move) {
+            "forward" -> Pair(acc_f + amount, acc_d)
+            "down" -> Pair(acc_f, acc_d + amount)
+            "up" -> Pair(acc_f, acc_d - amount)
+            else -> throw Exception()
+        }
+    }
+}
+
+private fun partTwo(): Triple<Int, Int, Int> {
+    return input().fold(Triple(0, 0, 0)) { (acc_f, acc_d, acc_aim), (move, amount) ->
+        when(move) {
+            "forward" -> Triple(acc_f + amount, acc_d + amount * acc_aim, acc_aim)
+            "down" -> Triple(acc_f, acc_d, acc_aim + amount)
+            "up" -> Triple(acc_f, acc_d, acc_aim - amount)
+            else -> throw Exception()
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    println(partOne().first * partOne().second)
+    println(partTwo().first * partTwo().second)
+}
